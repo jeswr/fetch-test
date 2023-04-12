13b96b8f5b18caf72c6ea138111e06720ffba7c0
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-04-12 12:04:32 +1000

    chore: get data

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 async function main() {
   try {
-    const res = await fetch('https://api.github.com/repos/eyereasoner/eye/releases/latest');
+    const res = await fetch('https://raw.githubusercontent.com/jeswr/fetch-test/main/data.txt');
     const text = await res.text();
     console.log(text.slice(0, 10), res.status);
     if (res.status !== 200) {
