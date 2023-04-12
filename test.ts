09ab326467af6b7603a8949423989c9407568c47
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-04-12 13:28:41 +1000

    chore: try with another webid

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@ import { fetch } from '@inrupt/universal-fetch';
 
 async function main() {
   try {
-    const res = await fetch('https://id.inrupt.com/jeswr');
+    const res = await fetch('https://id.inrupt.com/inrupte2erequestoresspodspaces');
     const text = await res.text();
     console.log(text, res.status);
     if (res.status !== 200) {
