11b4deac171b2bd2a57bba39eefd056d7899a24f
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-04-12 13:35:17 +1000

    chore: test against dev-next

--- test.ts
+++ test.ts
@@ -2,7 +2,7 @@ import { fetch } from '@inrupt/universal-fetch';
 
 async function main() {
   try {
-    const res = await fetch('https://id.inrupt.com/inrupte2erequestoresspodspaces');
+    const res = await fetch('https://id.dev-next.inrupt.com/inrupte2erequestoressdevnext');
     const text = await res.text();
     console.log(text, res.status);
     if (res.status !== 200) {
